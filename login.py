53dd271d1ff56c744f04c893c4dc7d40cf33c6de
Leonel Sangolquiza <97122219+Leonel90@users.noreply.github.com>
2022-07-16 08:59:48 -0500

    Python eliminado

--- login.py
+++
@@ -1,9 +0,0 @@
-class Login:
-    idLogin          = int
-    correo      = str
-    contraseña  = str
-    
-    def __init__(self, idLogin, correo, contraseña):
-        self.idLogin        = idLogin
-        self.correo         = correo
-        self.contraseña     = contraseña
